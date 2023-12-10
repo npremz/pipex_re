a01c93330217843791caddeb7dabd61f319a9964
Nicolas Premont <ni.premont@gmail.com>
2023-12-10 10:04:01 +0100

    cleaning

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: npremont <npremont@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 12:00:41 by npremont          #+#    #+#             */
-/*   Updated: 2023/12/08 10:06:30 by npremont         ###   ########.fr       */
+/*   Updated: 2023/12/10 09:55:42 by npremont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 
 void	ft_free_split(char **split);
 void	ft_free(void *ptr);
+char	*getpath(char *cmd, char **envp);
 void	exec(char *arg, char **envp);
 void	ft_error(void);
 
